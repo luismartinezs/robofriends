6f644624beb5661ad0244041afe141bedd1f0bba
Luis Martinez <martinez.s.luis@gmail.com>
2019-06-30 20:38:32 +0200

    More tests on CounterButton

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -16,4 +16,9 @@ it("correctly increments the counter", () => {
 
   wrapper.find('[id="counter"]').simulate("click");
   expect(wrapper.state()).toEqual({ count: 1 });
+  wrapper.find('[id="counter"]').simulate("click");
+  expect(wrapper.state()).toEqual({ count: 2 });
+  wrapper.find('[id="counter"]').simulate("keypress");
+  expect(wrapper.state()).toEqual({ count: 2 });
+  expect(wrapper.props().color).toEqual("red");
 });
