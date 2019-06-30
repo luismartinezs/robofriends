016a103c3fbea343d2858254e08b8690721a0bf4
Luis Martinez <martinez.s.luis@gmail.com>
2019-06-30 19:08:33 +0200

    Describe shallow, mount and render

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { shallow, mount, render } from "enzyme";
 // most of the time use shallow
+// mount: full DOM render, requires full DOM API (JS DOM, headless browser, actual browser...)
+// render: render components to static HTML (using cheerio lib), renders any children of current component (unlike shallow)
 import Card from "./Card";
 
 it("expect to render Card component", () => {
