a87c74f3e4c456ca1f382dd57569092b55fe3e98
Luis Martinez <martinez.s.luis@gmail.com>
2019-06-30 20:09:55 +0200

    Add counter button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import CounterButton from "./CounterButton";
 
 class Header extends Component {
   shouldComponentUpdate(nextProps, nextState) {
     return false;
   }
   render() {
-    console.log("Header");
-    return <h1 className="f2">Robofriends</h1>;
+    return (
+      <div>
+        <h1 className="f1">RoboFriends</h1>
+        <CounterButton />
+      </div>
+    );
   }
 }
+
 export default Header;
