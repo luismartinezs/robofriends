aa27ff440d7d7fd6647ef022a99cb2d03a9c5025
Luis Martinez Suarez <luis@Luiss-MBP.fritz.box>
2019-07-21 23:08:15 +0200

    pretty please yet again

--- src/components/CounterButton2.js
+++ src/components/CounterButton2.js
@@ -1,38 +1,31 @@
 import React, { Component } from "react";
 
 class CounterButton2 extends Component {
-
-
-constructor(props) {super(props);
-  this.state = {
-    count: 0,
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
     };
   }
 
-
-
   shouldComponentUpdate(nextProps, nextState) {
     if (this.state.count !== nextState.count) {
-  
-  
       return true;
-  
     }
     return false;
   }
 
   render() {
-  return (<button id="counter"
-     color={this.props.color} onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-  >
-
-
-     Count: {this.state.count}
+    return (
+      <button
+        id="counter"
+        color={this.props.color}
+        onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+      >
+        Count: {this.state.count}
       </button>
     );
   }
-
-
 }
 
 export default CounterButton2;
